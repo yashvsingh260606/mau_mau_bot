a641224e181eff79f5b2d82fcd1f43b481e475a3
yashvsingh260606 <luckystoreagra@gmail.com>
2025-08-20 17:36:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+import os
 import logging
 from keepalive import keep_alive
 keep_alive()
